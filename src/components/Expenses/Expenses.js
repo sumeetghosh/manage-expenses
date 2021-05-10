a8f28f1c2478727b8d7147e2c422107faa5fa38b
Sumeet Ghosh <sumeetghosh1990@gmail.com>
2021-05-10 17:55:52 +0530

    Chart Section Added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
-import ExpensesFilter from "./ExpensesFilter";
-
 import Card from "../UI/Card";
+
+import ExpensesFilter from "./ExpensesFilter";
+import ExpenseChart from "../Expenses/ExpensesChart";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -24,16 +25,9 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => {
-          return (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          );
-        })}
+        <ExpenseChart expenses={filteredExpenses} />
+
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
